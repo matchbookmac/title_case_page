b85efb4c6534196c726aecb982252a7621d0571b
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 09:37:49 -0700

    Capitalize first character of a word.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -2,6 +2,7 @@ class String
   define_method(:title_case) do
     word = []
     word = self.split(//)
-    word[0].capitalize()
+    word[0].capitalize!()
+    word.join()
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,7 +5,7 @@ describe('String#title_case') do
   it("will capitalize a character") do
     expect("a".title_case()).to(eq("A"))
   end
-  it("will capitalize a word") do
-    expect("path".title_case()).to(eq("Path"))    
+  it("will capitalize the first character of a word") do
+    expect("path".title_case()).to(eq("Path"))
   end
 end
