16aa48eb574bc9cd1b0eb19df90246f38e317d93
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 10:46:00 -0700

    does not ignore words like 'and'

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -11,4 +11,7 @@ describe('String#title_case') do
   it("will capitalize the first character of each word in a sentence") do
     expect("little red riding hood".title_case()).to(eq("Little Red Riding Hood"))
   end
+  it("will capitalize the first character of each word in a sentence") do
+    expect("little red riding hood and game of thrones".title_case()).to(eq("Little Red Riding Hood and Game of Thrones"))
+  end
 end
