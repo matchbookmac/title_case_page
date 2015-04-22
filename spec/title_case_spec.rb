52586bc86d2cb7c82ccd11c0408c5c889c977400
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 09:32:42 -0700

    does not capitalize full word

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -3,6 +3,9 @@ require('title_case')
 
 describe('String#title_case') do
   it("will capitalize a character") do
-    expect("a".title_case()).to(eq("A"))    
+    expect("a".title_case()).to(eq("A"))
+  end
+  it("will capitalize a word") do
+    expect("path".title_case()).to(eq("Path"))    
   end
 end
