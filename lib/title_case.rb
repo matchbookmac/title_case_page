acfe6dfa554fe7938ed947f1455c670bed238cce
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 10:47:08 -0700

    removes instead of ignoring words like 'and'

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,15 +1,15 @@
 class String
   define_method(:title_case) do
-    #dont_capitalize = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "it", "or", "nor"]
+    dont_capitalize = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "it", "or", "nor"]
     sentence = []
     sentence = self.split(" ")
     sentence.map!() do |word|
-      #if(dont_capitalize.include?(word))
-      #else
+      if(dont_capitalize.include?(word))
+      else
         word = word.split("")
         word[0].capitalize!()
         word = word.join()
-      #end
+      end
     end
     sentence.join(" ")
   end
